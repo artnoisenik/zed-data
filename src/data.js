468a050a9e49fbe64baf6c7aed72469cf1f5c331
Phil Skaggs <prskaggsjr@gmail.com>
2021-10-07 11:37:16 -0500

    Create data.js to fill with pretend horse data.

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const horses = [
+    {
+        name: 'Brave Cat',
+        type: 'mare',
+        osNum: 8,
+        osRaceAv: 32,
+        osWinPerc: 8.23,
+        osPlacePerc: 14.32,
+        breedDate: 09/20/21,
+        stableName: 'Winter Band'
+    },
+
+];
